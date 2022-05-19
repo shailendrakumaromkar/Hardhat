57ff14ae0207412d118fa0562a521d7c462ef9f8
eur\om <om@softwareag.com>
2022-05-19 22:51:17 +0530

    Hardhat first commit

--- test/Maths-Test.js
+++ test/Maths-Test.js
@@ -8,8 +8,9 @@ describe("Maths", function(){
         await maths.deployed();
 
         const sum = await maths.add(3,6);
+        console.log("printing before wait : ", sum);
         await sum.wait();
-
+        console.log("printing after wait : ", sum);
         //expect(await res).to.equal(9);
 //assert.equal(await maths.getRes(),9, "added correctly");
         assert.equal(sum,9, "added correctly");

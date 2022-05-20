a2b9a04557335446bcdf86a052177edd536ce446
eur\om <om@softwareag.com>
2022-05-21 01:07:21 +0530

    ERC20 Token

--- scripts/deploy_erc20OM.js
+++ scripts/deploy_erc20OM.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main(){
     const OmERC20 = await hre.ethers.getContractFactory("OmERC20");
-    const omerc20 = await OmErc20.deploy("1000000000000000000000000000");
+    const omerc20 = await OmERC20.deploy("1000000000000000000000000000");
     await omerc20.deployed();
 
     console.log("Om Token is deployed", omerc20.address);

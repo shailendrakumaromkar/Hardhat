bef2bc5fd94b882a7e55795170f986d31cefdc81
eur\om <om@softwareag.com>
2022-05-21 02:03:56 +0530

    ERC20 Token compatible

---
+++ contracts/SafeMath.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+library SafeMath{
+
+    function add(uint a, uint b) public pure returns (uint){
+        uint c= a+b;
+        require(c>=b);
+        return c;
+    }
+
+    function sub(uint a, uint b) public pure returns (uint){
+        require(b<=a);
+        uint c= a-b;
+        return c;
+    }
+}

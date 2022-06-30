a4259719c8fe7f47c741f1f64d39e7f24c64953a
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-06-30 17:09:35 +0530

    updated test case

--- contracts/OmERC20.sol
+++ contracts/OmERC20.sol
@@ -10,7 +10,7 @@ contract OmERC20 is ERC20Capped, Ownable{
     constructor(uint256 cap) ERC20("OmToken","OM") ERC20Capped (cap){
     }
 
-    //mint function for tokens
+    //mint function for tokensss
     function issueToken()public onlyOwner{
         _mint(msg.sender, 1000*10**18);
     }
